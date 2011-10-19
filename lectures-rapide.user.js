063b4904589197cb74787a7fef4097bc5d3a300f
Julien Barnier <julien@nozav.org>
2011-10-19 16:28:28 +0200

    Import initial

---
+++ lectures-rapide.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @id             lectures-rapide
+// @name           Lectures rapide
+// @version        0.1
+// @namespace      lectures-rapides
+// @author         Julien Barnier
+// @description    Améliorations de l'interface d'édition de Lectures
+// @include        http://lectures.revues.org/lodel/edition/*
+// @run-at         document-end
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js 
+// ==/UserScript==
+
+
