5a021fb471ae4d46e2a19b9a4bcc440434bb6b36
Julien Barnier <julien@nozav.org>
2017-12-21 10:12:54 +0100

    Numéro de version

--- lectures-rapide.user.js
+++ lectures-rapide.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             lectures-rapide
 // @name           Lectures rapide
-// @version        0.1.8
+// @version        0.1.9
 // @namespace      lectures-rapides
 // @author         Julien Barnier
 // @description    Améliorations de l'interface d'édition de Lectures
